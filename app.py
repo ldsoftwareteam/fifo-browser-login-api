3862d53645b310a9832b99b0e056356cbd35126a
mjainLD <mjain@lewanddowski.com>
2023-11-07 12:44:15 +0530

    Update App.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 import mysql.connector
 import bcrypt
@@ -63,4 +64,5 @@ def login():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    # app.run(debug=True)
